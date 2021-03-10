1da07db57ff93e25912f0a7371056f6337fe6229
Alex-CSse <alex.csse@gmail.com>
2021-03-09 22:52:04 -0300

    To Print

---
+++ print.c
@@ -0,0 +1,25 @@
+// Program to print even or odd
+#include <stdio.h>
+
+int main(void)
+{
+    int firstPage, lastPage;
+    
+    printf("Type first page: ");
+    scanf("%i", &firstPage);
+    printf("Type first page: ");
+    scanf("%i", &lastPage);
+    printf("\n");
+
+    int i = firstPage;
+    while (i < lastPage && i > 0)
+    {
+        printf("%i, ", i);
+        i = i + 2;
+    }
+    if (i == lastPage)
+    {
+        printf("%i\n\n", i);
+    }
+    return 0;
+}
---
+++ printing.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main (void)
+{
+    int firstPage, lastPage;
+
+    std::cout << "Type fist page: ";
+    std::cin >> firstPage;
+    std::cout << "Type last page: ";
+    std::cin >> lastPage;
+    std::cout << "\n";
+
+    int i = firstPage;
+    while (i < lastPage && i > 0)
+    {
+        std::cout << i << ", ";
+        i = i + 2;
+    }
+    if (i == lastPage)
+    {
+        std::cout << i << "\n\n";
+    }
+    return 0;
+}
